4ab3653eecd3c4ecf595fd39dbc6fa7aeecea9b4
The-_-onE <834489218@qq.com>
2015-10-17 22:57:47 +0800

    New GifView Test

--- app/src/main/java/com/xmx/wenote/ChoosePhoto/BigPhotoActivity.java
+++ app/src/main/java/com/xmx/wenote/ChoosePhoto/BigPhotoActivity.java
@@ -6,17 +6,22 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+import com.xmx.wenote.ChoosePhoto.entities.GifView;
 import com.xmx.wenote.R;
 
 public class BigPhotoActivity extends Activity {
-    ImageView iv_show_big_pic;
+    GifView iv_show_big_pic;
+    ImageView iv_show_big_iv;
     String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cp_big_photo_activity);
-        iv_show_big_pic = (ImageView) findViewById(R.id.iv_show_big_pic);
+        iv_show_big_pic = (GifView) findViewById(R.id.iv_show_big_pic);
+        iv_show_big_pic.setMovieResource(R.drawable.gif);
+        iv_show_big_iv = (ImageView) findViewById(R.id.iv_show_big_iv);
+        iv_show_big_iv.setImageResource(R.drawable.ic_launcher);
         path = getIntent().getStringExtra("path");
         //iv_show_big_pic.setImageBitmap(BitmapFactory.decodeFile(path));
     }
